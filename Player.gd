9890b3234f757b4bc3cc4b9572a25786d16291ea
Brandon Blodget <brandon.blodget@gmail.com>
2020-06-27 17:22:08 -0700

    Updating movement of drop_test.

--- Player.gd
+++ Player.gd
@@ -1,10 +1,22 @@
 extends KinematicBody2D
 
 
-const GRAVITY = 200
+const GRAVITY = 2
+const MAX_SPEED = 400
 
-var motion = Vector2.ZERO
+var velocity = Vector2.ZERO
+
+
+func get_input():
+	var input_vector = Vector2.ZERO
+	
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up") 
+	return input_vector
 
 func _physics_process(delta):
-	motion.y += GRAVITY * delta
-	motion = move_and_slide(motion, Vector2.UP)
+	var input_vector = get_input()	
+	# gravity_velocity.y += GRAVITY
+	velocity = input_vector
+	var tmp_vel = move_and_collide(velocity * delta * MAX_SPEED)
+
