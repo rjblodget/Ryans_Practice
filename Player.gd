f402cd57dccca89dab80347cae23be59bba01504
Brandon Blodget <brandon.blodget@gmail.com>
2020-06-30 17:56:35 -0700

    Got the animations working in drop_test.

--- Player.gd
+++ Player.gd
@@ -13,25 +13,27 @@ const WALK_MAX_SPEED = 200
 # Global Vars
 var velocity = Vector2.ZERO
 
-func _input(event):
-	if event is InputEventKey:
-		if event.scancode == KEY_LEFT:
-			$OurSprite.play("walk")
-		elif event.scancode == KEY_RIGHT:
-			$OurSprite.play("walk")
-		elif event.scancode == KEY_UP:
-			$OurSprite.play("Up")
-		elif event.scancode == KEY_SPACE:
-			$OurSprite.play("Up")
+#func _input(event):
+#	if Input.
+#	if event is InputEventKey:
+#		if event.scancode == KEY_LEFT:
+#			$OurSprite.play("walk")
+#		elif event.scancode == KEY_RIGHT:
+#			$OurSprite.play("walk")
+#		elif event.scancode == KEY_UP:
+#			$OurSprite.play("Up")
+#		elif event.scancode == KEY_SPACE:
+#			$OurSprite.play("Up")
 	
 
 
 func _physics_process(delta):
 	var walk = WALK_FORCE * (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"))	
+	var is_on_platform = is_on_floor()
 	
 	# Slow down the player if they're not trying to move.
 	if walk == 0:
-		if is_on_floor():
+		if is_on_platform:
 			# The velocity, slowed down a bit, and then reassigned.
 			velocity.x = move_toward(velocity.x, 0, STOP_FORCE * delta)
 	else:
@@ -39,22 +41,34 @@ func _physics_process(delta):
 		
 	# Clamp to the maximum horizontal movement speed.
 	velocity.x = clamp(velocity.x, -WALK_MAX_SPEED, WALK_MAX_SPEED)
-	
-	# select walk animiation
-	if velocity.x <0:
-		$OurSprite.flip_h = true
-	else:
-		$OurSprite.flip_h = false
-		
+			
 	
 	# Vertical movement code. Apply gravity.
 	velocity.y = velocity.y + (GRAVITY * delta)
 	
 	# Check for jumping. is_on_floor() must be called after movement code.
-	if is_on_floor() and Input.is_action_just_pressed("jump"):
+	if is_on_platform and Input.is_action_just_pressed("jump"):
 		velocity.y = -JUMP_SPEED
+		
 	
+	# select  nimiation
+	if is_on_platform:
+		# On the platform
+		if velocity.x == 0:
+			$OurSprite.stop()
+		elif velocity.x > 0 :
+			$OurSprite.play("walk")
+			$OurSprite.flip_h = false
+		else:
+			$OurSprite.play("walk")
+			$OurSprite.flip_h = true	
+	else:
+		# In the air
+		$OurSprite.play("Up")
+		
+		
 	# Move based on the velocity and snap to the ground.
 	velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
+		
 	
 
